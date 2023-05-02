6f10003cce891104016747c822def4d27569a982
mohamed reda <aboreda132132@gmail.com>
2023-05-02 16:14:11 -0700

    up to

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,10 +14,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		printf("%d\n",h->n);
-		number++;
-		h = h->next;
+	printf("%d\n", h->n);
+	number++;
+	h = h->next;
 	}
-
-	return (number);	
+return (number);
 }
